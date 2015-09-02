4ba8f7f3e98d6da164a8f5a8bb05824545d634ed
Spas Vutov <VutovSpas@gmail.com>
2015-09-02 21:31:05 +0300

    Added intergration tests for reply controller

--- TweeterLike.Tests/Mock/MockContainer.cs
+++ TweeterLike.Tests/Mock/MockContainer.cs
@@ -3,8 +3,8 @@
     using System.Collections.Generic;
     using System.Linq;
     using Data.Repositories;
-    using Models.DbModels;
     using Moq;
+    using TweeterLike.Models.DbModels;
 
     public class MockContainer
     {
---
+++ TweeterLike.Tests/Models/LoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TweeterLike.Tests.Models
+{
+    public class LoginDto
+    {
+        public string Access_Token { get; set; }
+
+        public string Username { get; set; }
+    }
+}
